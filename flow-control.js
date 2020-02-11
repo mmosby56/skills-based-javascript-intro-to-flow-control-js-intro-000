ab9c5da0d8588c6f770afb7eb2555a7c051fd06c
mark mosby <markemosby@msn.com>
2020-02-11 18:13:30 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,9 +1,11 @@
 function basicTeenager(age)
-if (age >= 13 && age <= 19)
+if (13 <= (age) && (age) <= 19)
 {
 return 'You are a teenager!'}
-}
 
+else {
+  return 'You are not a teenager'
+}
 
 function teenager(age) {
 
